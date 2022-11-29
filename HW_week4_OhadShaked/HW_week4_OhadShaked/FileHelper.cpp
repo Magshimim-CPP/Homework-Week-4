29a950d69fa3b20912076983654f017a24b45595
Ohad Shaked <ohad.saked@gmail.com>
2022-11-29 17:35:04 +0200

    added documentation to all header and code files

--- HW_week4_OhadShaked/HW_week4_OhadShaked/FileHelper.cpp
+++ HW_week4_OhadShaked/HW_week4_OhadShaked/FileHelper.cpp
@@ -1,85 +1,104 @@
 #include "FileHelper.h"
 
+/*
+Static function recives a file's name, opens it, reads the file's content to a string, and returns the string.
+Input: string fileName
+Output: string result
+*/
 string FileHelper::readFileToString(string fileName)
 {
-	string result;
-	ifstream file;
-	char ch;
+	string result; //string for the result
+	ifstream file; //file stream variable (read)
+	char ch; //char for the current letter when reading the file
 
-	file.open(fileName);
+	file.open(fileName); //opening the file.
 
-	if (file)
+	if (file) //if a file with the given name exists 
 	{
-		while (file)
+		while (file) //looping until the end of the file.
 		{
-			file.get(ch);
+			file.get(ch); //reading the current letter
 
-			if (file)
+			if (file) //checking if file reached end of file after reading the letter (entering if it didn't reach EOF).
 			{
-				result += ch;
+				result += ch; //adding the current char to the result.
 			}
 
 		}
 
-		file.close();
+		file.close(); //closing the file.
 	}
+
+	//else, if a file with the given name dose not exists 
 	else
 	{
-		result = FILE_NOT_FOUND;
+		result = FILE_NOT_FOUND; //setting the result to a error message string.
 	}
 
-	return result;
+	return result; //returning the result string.
 }
 
+/*
+Static function writes the content of one file to another file.
+Input: string inputFileName, string outputFileName
+Output: none
+*/
 void FileHelper::writeWordsToFile(string inputFileName, string outputFileName)
 {
-	string result;
-	ifstream inFile;
-	ofstream outFile;
+	string line; //string for storing the current line when reading the file.
+	ifstream inFile; //file stream variable (read)
+	ofstream outFile; //file stream variable (write)
 
+	//opening the files
 	outFile.open(outputFileName);
 	inFile.open(inputFileName);
 
-	if (outFile && inFile)
+	if (outFile && inFile) //entering condition if both files exist.
 	{
-		while (inFile.good())
+		while (inFile.good()) //checking if file's stream is good (if it raised any error or not).
 		{
-			inFile >> result;
-			outFile << result;
+			inFile >> line; //reading the current line
+			outFile << line; //entering the line to the output file
 
-			if (inFile.good())
+			if (inFile.good()) //checking if file's stream is still good after reading the letter (if it raised any error or not).
 			{
 				outFile << '\n';
 			}
 		}
 
+		//closing the files
 		inFile.close();
-		outFile.close();
+		outFile.close(); 
 	}
-	else
+	else //if one or more files dosent exist.
 	{
-		cout << ONE_OR_MORE_FILES_NOT_FOUND << endl;
+		cout << ONE_OR_MORE_FILES_NOT_FOUND << endl; //printing error message.
 	}
 }
 
+/*
+Static function saves a string text inside the output file
+Input: string text, string outputFileName
+Output: none
+*/
 void FileHelper::saveTextInFile(string text, string outputFileName)
 {
-	ofstream outFile;
+	ofstream outFile; //file stream variable (write)
 
-	outFile.open(outputFileName);
+	outFile.open(outputFileName); //opening the file.
 
-	if (outFile)
+	if (outFile) //if a file with the given name exists 
 	{
-		for (int i = ZERO; i < text.length(); i++)
+		for (int i = ZERO; i < text.length(); i++) //looping for each letter of the text.
 		{
-			outFile << text[i];
+			outFile << text[i]; //inserting the current letter to the output file.
 		}
 
-		outFile.close();
+		outFile.close(); //closing the file.
 	}
 
-	else
+	else //if a file with the given name dosent exist
 	{
-		cout << FILE_NOT_FOUND << endl;
+		cout << FILE_NOT_FOUND << endl; //printing error message
 	}
 }
--- HW_week4_OhadShaked/HW_week4_OhadShaked/FileHelper.h
+++ HW_week4_OhadShaked/HW_week4_OhadShaked/FileHelper.h
@@ -1,29 +1,34 @@
 #pragma once
-#pragma once
 #ifndef FILEHELPER_H
 #define FILEHELPER_H
 
+//Using iostream, string, fstream, and PlainText header file.
 #include <iostream>
 #include <string>
 #include <fstream>
 #include "PlainText.h"
 
+//Using specific functions from the std namespace.
 using std::cout;
 using std::endl;
 using std::string;
 using std::ifstream;
 using std::ofstream;
 
+//Defining constant values for ZERO (0)
 #define ZERO 0
+
+//Defining text errors in case a file was not found or could not be opend.
 #define FILE_NOT_FOUND "ERROR: File was not found or was unable to open!"
 #define ONE_OR_MORE_FILES_NOT_FOUND "ERROR: One or more file were not found or was unable to open!"
 
+//File Helper Class.
 class FileHelper
 {
 public:
-	static string readFileToString(string fileName);
-	static void writeWordsToFile(string inputFileName, string outputFileName);
-	static void saveTextInFile(string text, string outputFileName);
+	static string readFileToString(string fileName); //Static function recives a file's name, opens it, reads the file's content to a string, and returns the string.
+	static void writeWordsToFile(string inputFileName, string outputFileName); //Static function writes the content of one file to another file.
+	static void saveTextInFile(string text, string outputFileName); //Static function saves a string text inside the output file.
 };
 
 
